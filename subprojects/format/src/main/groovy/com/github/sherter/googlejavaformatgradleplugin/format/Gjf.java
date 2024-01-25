330b592c62681550d1feca9324caefc57504558c
Thomas Keller <thomas.keller@external.gls-group.com>
2024-01-25 18:25:10 +0100

    Bump version, fix maven publish

--- subprojects/format/src/main/groovy/com/github/sherter/googlejavaformatgradleplugin/format/Gjf.java
+++ subprojects/format/src/main/groovy/com/github/sherter/googlejavaformatgradleplugin/format/Gjf.java
@@ -9,8 +9,10 @@ public class Gjf {
   public static final String ARTIFACT_ID = "google-java-format";
 
   public static final ImmutableList<String> SUPPORTED_VERSIONS =
-      ImmutableList.of("1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10.0", "1.11.0", "1.12.0",
-              "1.13.0", "1.14.0", "1.15.0", "1.16.0", "1.17.0", "1.18.0", "1.18.1", "1.19.0", "1.19.1", "1.19.2");
+      ImmutableList.of(
+          "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10.0", "1.11.0",
+          "1.12.0", "1.13.0", "1.14.0", "1.15.0", "1.16.0", "1.17.0", "1.18.0", "1.18.1", "1.19.0",
+          "1.19.1", "1.19.2");
 
   /**
    * Constructs a new formatter that delegates to <a
